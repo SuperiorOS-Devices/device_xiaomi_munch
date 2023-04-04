b459b90dde6f08c8e38bba1c2e187d8e8257dcac
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-04-04 20:55:18 +0530

    Superior OS bring-up

--- superior_munch.mk
+++ superior_munch.mk
@@ -8,15 +8,36 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-DEVICE_MAINTAINER := madmax7896
+# Officialify
+SUPERIOR_OFFICIAL := true
 
-PRODUCT_NAME := arrow_munch
+# Maintainer Flag (one word only/no spacing)
+MAINTAINER := Mudit
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Supported Device Flags
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+#Superior OS Flags
+TARGET_INCLUDE_PIXEL_CHARGER := true
+BUILD_WITH_GAPPS := true
+USE_QUICKPIC := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
+
+# Quick Tap Feature
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := superior_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
